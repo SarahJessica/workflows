655505b617728878b5918805df0d8ded085aa91b
System Administrator <sarahjessicaleivers@gmail.com>
2015-06-23 11:53:24 +0100

    added coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function (){
-	gutil.log('this is working');
+var coffeeSrcs = ['components/coffeescript/*.coffee'];
+
+gulp.task('coffeeConv', function(){
+	gulp.src(coffeeSrcs)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
+
+// gulp.task('log', function (){
+// 	gutil.log('this is working');
+// });
