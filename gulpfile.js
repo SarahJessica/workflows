4d42b4a10b59513e17cfb798767df88ff9b5cee0
System Administrator <sarahjessicaleivers@gmail.com>
2015-06-30 00:45:35 +0100

    concatinated js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
-var coffeeSrcs = ['components/coffeescript/*.coffee'];
+var coffeeSrcs = ['components/coffeescript/rclick.coffee'];
+var jsSrcs = [	'components/scripts/*.js', 
+				// 'components/scripts/tagline.js', 
+				// 'components/scripts/template.js',
+				// 'components/scripts/rclick.js'
+			];
 
 gulp.task('coffeeConv', function(){
 	gulp.src(coffeeSrcs)
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSrcs)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
 
 // gulp.task('log', function (){
